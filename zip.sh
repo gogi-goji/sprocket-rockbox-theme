9d014628a9d347ca7784898e780b06a696b1f3bf
Cam Mendoza <mendoza.cam@gmail.com>
2013-02-28 14:03:57 -0700

    Stuff for packaging things up

---
+++ zip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: Expand script with checks to make sure .rockbox/ exists and maybe that
+# this is being run from the right directory
+
+# This is a really simple, mostly just a reminder so I don't forget the correct
+# command to zip up the project with the right structure
+
+zip -ru sprocket.zip .rockbox/
